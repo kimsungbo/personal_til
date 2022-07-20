37878b18bc5da0c863e4c2580de6bedf212ffac3
kimsungbo <kimsungbo97@gmail.com>
2022-07-21 01:41:19 +0900

    20220721 Kotlin Love Type Text (using navigation jetpack / setOnClickListener interface)

---
+++ kotlin_practice/app/src/main/java/com/sungbo/kotlin_lovetest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sungbo.kotlin_lovetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sungbo.kotlin_practice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
